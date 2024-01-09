2454f2d46bf168b6f3d3229969852b0471f7fa52
sevvaltufekci <sevval.kiransoy@gmail.com>
2024-01-09 21:32:21 +0100

    Header locatelerini ekledim, kontrolünü rica ederim.

--- src/test/java/com/easybusticket/pages/HomePage.java
+++ src/test/java/com/easybusticket/pages/HomePage.java
@@ -14,22 +14,69 @@ public class HomePage extends BasePage{
         PageFactory.initElements(Driver.get("stage"),this);
     }
 
+    //Cookie Allow Button
     @FindBy(xpath = "//a[text()='Allow']")
     public WebElement cookies;
 
+    //Phone Button
+    @FindBy(xpath = "//a[@href=''][1]")
+    public WebElement phoneLink;
+
+    //Twitter Icon Button
+    @FindBy(xpath = "//a[@href='https://www.twitter.com']")
+    public WebElement twitterIcon;
+
+    //Facebook Icon Button
+    @FindBy(xpath = "//a[@href='https://www.facebook.com']")
+    public WebElement facebookIcon;
+
+
+    //Youtube Icon Button
+    @FindBy(xpath = "//a[@href='https://www.youtube.com']")
+    public WebElement youtubeIcon;
+
+    //Instagram Icon Button
+    @FindBy(xpath = "//a[@href='https://www.instagram.com']")
+    public WebElement instagramIcon;
+
+    //SignIn Button
+    @FindBy(xpath = "//*[@class='sign-in']")
+    public WebElement signInLink;
+
+    //SignUp Button
     @FindBy(xpath = "//*[@class='sign-up']")
     public WebElement signUpLink;
 
 
+    //Logo ButtonLink
+    @FindBy(xpath = "//div[@class='logo']")
+    public WebElement logoButton;
 
-    @FindBy(xpath = "//input[@id='firstname']")
-    public WebElement textBoxOfFirstName;
+    //Home Title Button
+    @FindBy(xpath = "(//a[text()='Home'])")
+    public WebElement homeTitle;
 
-    @FindBy(xpath = "//input[@type='checkbox']")
-    public WebElement buttonOfAcceptingall;
+    //About Title Button
+    @FindBy(xpath = "(//a[text()='About'])")
+    public WebElement aboutTitle;
 
-    @FindBy(xpath = "//button[text()='Sign Up']")
-    public WebElement buttonOfSignUp;
+    //FAQs Title Button
+    @FindBy(xpath = "(//a[text()='FAQs'])")
+    public WebElement faqsTitle;
+
+    //Blog Title Button
+    @FindBy(xpath = "(//a[text()=Blog])")
+    public WebElement blogTitle;
+
+    // Contact Title Button
+    @FindBy(xpath = "(//a[text()='Contact'])")
+    public WebElement contactTitle;
+
+    //Buy Ticket ButtonLink
+    @FindBy(xpath = "//div[@class='d-flex flex-wrap algin-items-center']")
+    public WebElement buyTicketButtonLink;
+
+
 
     public void acceptCookies(){
         waitAndClick(cookies);
@@ -38,6 +85,7 @@ public class HomePage extends BasePage{
     public Register clickSignUp (){
             waitAndClick(signUpLink);
             return new Register();
+
     }
 
 }
