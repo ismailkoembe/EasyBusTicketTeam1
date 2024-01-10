6c62067e966b6f67db4142b77be0a3bb35e45b75
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-01-10 14:02:09 +0000

    abouttitle ismini aboutButton olarak update ettim.About Page kısmına tıklama ve sayfaya gittiğini doğrulama testi yazıldı

---
+++ src/test/java/com/easybusticket/pages/AboutPage.java
@@ -0,0 +1,27 @@
+package com.easybusticket.pages;
+
+import com.easybusticket.utilities.Driver;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+@Slf4j
+public class AboutPage extends BasePage{
+    public AboutPage(){
+        PageFactory.initElements(Driver.get("stage"),this);
+    }
+    //About page title
+    @FindBy (xpath = "(//h2[@class='title'])[1]")
+    public WebElement labelAboutPage;
+
+    public void titleCheckTest(){
+        String expectedAboutTitle = "Easy Bus Ticket - About";
+        String actualAboutTitle = Driver.get(env).getTitle();
+        softAssert.assertEquals(actualAboutTitle,expectedAboutTitle);
+        softAssert.assertAll();
+
+    }
+
+
+}
