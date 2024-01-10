ba9ff5ea6b9036849135fabc9f2d0fb9d50a35a7
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-01-10 20:25:57 +0000

    logout method yazıldı.

---
+++ src/test/java/com/easybusticket/tests/US19_TC01.java
@@ -0,0 +1,22 @@
+package com.easybusticket.tests;
+
+import com.easybusticket.pages.HomePage;
+import com.easybusticket.pages.UserDashboard;
+import com.easybusticket.pages.UserLoginPage;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+@Slf4j
+public class US19_TC01 extends BaseTest {
+
+    @Test
+    public void userLogoutTest(){
+        // navigate to the login page
+        UserLoginPage userLoginPage = new HomePage().clickToSignIn();
+
+        //navigate to the dashboard page
+        UserDashboard userDashboard = userLoginPage.login();
+
+        //navigate to the login page
+        UserLoginPage userLogout = new UserDashboard().logout();
+    }
+}
