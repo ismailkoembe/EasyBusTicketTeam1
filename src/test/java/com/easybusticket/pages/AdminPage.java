e3501c27c1ee8006875245fcbe149b551b7dcd4c
reyhany <breyhanyildiz@gmail.com>
2024-01-11 12:50:52 +0100

    update/adminpage/basetest/us21/11/01/24

--- src/test/java/com/easybusticket/pages/AdminPage.java
+++ src/test/java/com/easybusticket/pages/AdminPage.java
@@ -1,13 +1,16 @@
 package com.easybusticket.pages;
 
 import com.easybusticket.utilities.Driver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AdminPage {
-    public AdminPage(){
-        PageFactory.initElements(Driver.get("stage"),this);
+public class AdminPage extends BasePage {
+
+
+    public AdminPage() {
+        PageFactory.initElements(Driver.get("stage"), this);
     }
 
     //Username box for Admin
@@ -24,12 +27,35 @@ public class AdminPage {
 
 
     //WelcomeToEasyBusTicket text
-    @FindBy(xpath = "//*[@class='logo-text mb-15']")
-    public WebElement WelcomeToEasyBusTicket;
+    @FindBy(xpath = "//*[text()='Admin Login to Easy Bus Ticket dashboard']")
+    public WebElement AdminLoginToEasyBusTicketDashboard;
 
     //Forgot Password Button
     @FindBy(xpath = "//*[@class='text-muted text--small']")
     public WebElement LinkForgotPassword;
+
+
+    public void AdminPage(String url) {
+        Driver.get("stage").get("AdminUrl");
+    }
+
+
+
+
+
+
+    public AdminDashboardPage adminLogin() {
+       //waitAndClick(AdminUsernameBox);
+        AdminUsernameBox.sendKeys("admin07");
+        AdminPasswordBox.sendKeys("123123123");
+        waitAndClick(ButtonAdminLogin);
+
+        String expectedText = "Admin Login to Easy Bus Ticket dashboard";
+        String actualText = Driver.get(env).getTitle();
+        softAssert.assertEquals(actualText, expectedText);
+
+        return new AdminDashboardPage();
+    }
 
 
 }
---
+++ src/test/java/com/easybusticket/tests/BaseTestAdmin.java
@@ -0,0 +1,80 @@
+package com.easybusticket.tests;
+
+import com.easybusticket.utilities.Driver;
+import com.easybusticket.utilities.Environments;
+import com.easybusticket.utilities.PropManager;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.*;
+import org.testng.asserts.SoftAssert;
+
+import java.lang.reflect.Method;
+import java.util.Objects;
+
+/**
+ * @author Reyhan
+ */
+@Slf4j
+public class BaseTestAdmin {
+    public String env = Environments.STAGE.name;
+    public final Integer timeout = Integer.valueOf(Objects.requireNonNull(PropManager.getProperties(env, "timeout")));
+    public final SoftAssert softAssert=new SoftAssert();
+
+    @BeforeMethod(enabled = false)
+    public void setupUserContext(Method method) {
+        /**
+         * BaseTest class is essential part of the framework.
+         * In order to switch environment, BaseTest class holds central point.
+         */
+        log.info("Running tests on: " + env);
+    }
+
+
+    @Parameters({"masterPlan", "planToRun", "username", "apiKey"})
+    @AfterSuite(enabled = false)
+    public void init(@Optional("masterPlan") String masterPlan,
+                     @Optional("planToRun") String planToRun,
+                     @Optional("planToRun") String username,
+                     @Optional("planToRun") String apiKey) {
+        /**
+         * Not implemented just for demo purpose for interview
+         * When the Jenkins triggers framework or QA person run via terminal or xml file
+         * those injected parameters will be initialized.
+         * enabled = false because it is not implemented
+         * */
+
+    }
+
+
+    @Parameters({"masterPlan", "planToRun"})
+    @AfterSuite(enabled = false)
+    public void reportToTestRail(@Optional("masterPlan") String masterPlan,
+                                 @Optional("planToRun") String planToRun) {
+        /**
+         * Not implemented just for demo purpose for interview
+         * When all automation is done, the framework can report results in TestRail or
+         * some other platform
+         * enabled = false because it is not implemented
+         * */
+
+    }
+
+    @BeforeMethod
+    public void setup(){
+        Driver.get(env).get(PropManager.getProperties(env,"AdminUrl"));
+        Driver.get(env).manage().window().maximize();
+        log.info("Navigating {} environment {}", PropManager.getProperties(env,"AdminUrl"), env);
+    }
+
+
+    @AfterMethod
+    public void teardown(){
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Driver.closeDriver();
+    }
+
+}
+
