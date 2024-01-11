4020fa3e5474957a121e6b5c521c569f6447ea0c
Gulnur <taskin.gulnur@gmail.com>
2024-01-11 23:20:57 +0100

    US13_TC03 class has been created.

---
+++ src/test/java/com/easybusticket/tests/US13_TC03.java
@@ -0,0 +1,13 @@
+package com.easybusticket.tests;
+
+public class US13_TC03 extends BaseTest{
+
+    //Click on the "Buy tickets" link on the Dashboard page.
+    //The user is successfully directed to the ticket purchase screen.
+    //The departure point is selected from the 'pickup point' menu on the reservation page.
+    //Destination point is selected from the 'Dropping point' menu
+    //Travel date is selected from the 'Date of journey' box
+    //Vehicle has selected from the 'Vehicle type' menu.
+    //Route has selected from the 'Routes' menu.
+    //When press the 'Finds ticket' button, tickets suitable for filtering will be listed.
+}
