57704e8a5a341fdfb09a737842402728a747c184
aslierk <asliekmekc@gmail.com>
2024-01-13 00:40:52 +0100

    feat2:created new Page classes. feat3 working on US25

--- src/test/java/com/easybusticket/tests/PaymentHistoryAndOptionsViewerTest_US25.java
+++ src/test/java/com/easybusticket/tests/PaymentHistoryAndOptionsViewerTest_US25.java
@@ -2,6 +2,7 @@ package com.easybusticket.tests;
 
 import com.easybusticket.pages.AdminDashboardPage;
 import com.easybusticket.pages.AdminPage;
+import com.easybusticket.pages.PendingPaymentPage;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -17,22 +18,9 @@ public class PaymentHistoryAndOptionsViewerTest_US25 extends BaseTestAdmin {
 
         //log in as an administrator and locate the admin Dashboard.
         AdminDashboardPage adminDashboardPage = new AdminPage().adminLogin();
-        log.info("Admin logged in admin dashboards");
+        PendingPaymentPage pendingPaymentPage = new PendingPaymentPage();
 
-        //Verrify that the "Payment History" dropdown menu with items (Pending Payment, Successful Payments, Rejected Payment and All History) is  visible.
 
-        softAssert.assertTrue(adminDashboardPage.paymentHistoryDropdownDashboard.isDisplayed());
-        log.info("Payment History Dropdown menu is displayed.");
-        adminDashboardPage.paymentHistoryDropdownDashboard.click();
-
-        Select dropdown = new Select(adminDashboardPage.paymentHistoryDropdownDashboard);
-
-        softAssert.assertTrue(adminDashboardPage.isOptionPresent(dropdown, "Pending Payment"));
-        softAssert.assertTrue(adminDashboardPage.isOptionPresent(dropdown, "Successful Payment"));
-        softAssert.assertTrue(adminDashboardPage.isOptionPresent(dropdown, "Rejected Payment"));
-        softAssert.assertTrue(adminDashboardPage.isOptionPresent(dropdown, "All Payment"));
-
-        softAssert.assertAll();
     }
 }
 
