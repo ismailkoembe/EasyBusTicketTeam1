0d9d0d375fb5780a0eb27467b8d7f62bd6be43ef
Gamze Erdogan <ckrgamze@gmail.com>
2024-01-12 11:56:41 +0100

    logged in successfully

--- src/test/java/com/easybusticket/tests/US18_TC01_TC02.java
+++ src/test/java/com/easybusticket/tests/US18_TC01_TC02.java
@@ -1,7 +1,7 @@
 package com.easybusticket.tests;
 
 import com.easybusticket.pages.HomePage;
-import com.easybusticket.pages.UserDashboard;
+import com.easybusticket.pages.UserDashboardPage;
 import com.easybusticket.pages.UserLoginPage;
 import org.testng.annotations.Test;
 
@@ -9,7 +9,7 @@ public class US18_TC01_TC02 extends BaseTest{
     @Test
     public void changePasswordTest(){
         UserLoginPage userLoginPage = new HomePage().clickToSignIn();
-        UserDashboard userDashboard = userLoginPage.login();
+        UserDashboardPage userDashboard = userLoginPage.login("logintesti","LoginTesti!1");
 
     }
 }
