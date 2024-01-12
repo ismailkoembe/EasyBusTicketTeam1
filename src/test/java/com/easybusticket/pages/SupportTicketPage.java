a0e15e0b120490d2008bea7e6975618aabb5d582
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-01-12 16:48:05 +0000

    request history - new ticket created

--- src/test/java/com/easybusticket/pages/SupportTicketPage.java
+++ src/test/java/com/easybusticket/pages/SupportTicketPage.java
@@ -1,10 +1,16 @@
 package com.easybusticket.pages;
 
 import com.easybusticket.utilities.Driver;
+import com.easybusticket.utilities.PropManager;
 import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+
 @Slf4j
 public class SupportTicketPage extends BasePage{
     public SupportTicketPage() {
@@ -57,29 +63,81 @@ public class SupportTicketPage extends BasePage{
     @FindBy(xpath = "//a[text()='Requests']")
     public WebElement requestsOption;
 
+    @FindBy(xpath = "//a[@class='btn btn-sm bg-white float-right support-ticket']")
+    public WebElement mySupportTicketButton;
+
+    @FindBy(xpath = "//*[@name='name']")
+    public WebElement textBoxOfNameButton;
+
+    @FindBy(xpath = "//*[@name='email']")
+    public WebElement textBoxOfEmailButton;
+
+    @FindBy(xpath = "//input[@name='subject']")
+    public WebElement textBoxOfSubject;
+
+    @FindBy(xpath = "//*[@name='priority']")
+    public WebElement dropDownPriority;
+
+    @FindBy(xpath = "//*[@id='inputMessage']")
+    public WebElement textBoxOfMessage;
+
+    @FindBy(xpath = "//*[@class='fas fa-plus']")
+    public WebElement extraFileButton;
+
+    @FindBy(xpath = "//*[@class='btn btn--base h-40']")
+    public WebElement submitButton;
+
+    @FindBy(xpath = "//*[text()='High']")
+    public WebElement optionPriorityHigh;
+
+
 
 
     public void requestHistoryPageVerifyTest(){
-        columnSubject.isDisplayed();
-        columnStatus.isDisplayed();
-        columnPriority.isDisplayed();
-        columnLastReply.isDisplayed();
-        columnAction.isDisplayed();
-        newTicketButton.isDisplayed();
-        rowFirstRequest.isDisplayed();
-        actionButton.isDisplayed();
+        //columnSubject.isDisplayed();
+        softAssert.assertTrue(columnSubject.isDisplayed());
+        softAssert.assertTrue(columnStatus.isDisplayed());
+        softAssert.assertTrue(columnPriority.isDisplayed());
+        softAssert.assertTrue(columnLastReply.isDisplayed());
+        softAssert.assertTrue(columnAction.isDisplayed());
+        softAssert.assertTrue(newTicketButton.isDisplayed());
+        softAssert.assertTrue(rowFirstRequest.isDisplayed());
+        softAssert.assertTrue(actionButton.isDisplayed());
 
         waitAndClick(actionButton);
         log.info("Request Detail Page loaded");
-        labelOpenedRequestTitle.isDisplayed();
-        selectFileButton.isDisplayed();
-        replyButton.isDisplayed();
-        yourReplyBox.isDisplayed();
-        labelLastRequestMessage.isDisplayed();
+        softAssert.assertTrue(labelOpenedRequestTitle.isDisplayed());
+        softAssert.assertTrue(selectFileButton.isDisplayed());
+        softAssert.assertTrue(replyButton.isDisplayed());
+        softAssert.assertTrue(yourReplyBox.isDisplayed());
+        softAssert.assertTrue(labelLastRequestMessage.isDisplayed());
         waitAndClick(dropDownSupportRequest);
         waitAndClick(requestsOption);
+        softAssert.assertAll();
 
 
     }
 
+    public void requestHistoryNewTicketTest(){
+        waitAndClick(newTicketButton);
+        softAssert.assertTrue(mySupportTicketButton.isDisplayed());
+        softAssert.assertTrue(textBoxOfNameButton.isDisplayed());
+        softAssert.assertTrue(textBoxOfEmailButton.isDisplayed());
+        waitAndClick(textBoxOfSubject);
+        textBoxOfSubject.sendKeys(PropManager.getProperties(env, "us16Subject"));
+        waitAndClick(dropDownPriority);
+        actions.click(dropDownPriority).sendKeys(Keys.DOWN).click();
+        softAssert.assertTrue(textBoxOfMessage.isDisplayed());
+        textBoxOfMessage.sendKeys(PropManager.getProperties(env, "us16Message"));
+        softAssert.assertTrue(selectFileButton.isDisplayed());
+        softAssert.assertTrue(extraFileButton.isDisplayed());
+        waitAndClick(submitButton);
+
+        String expectedRequestHistoryPageUrl="https://qa.easybusticket.com/ticket";
+        String actualRequestHistoryPageUrl=Driver.get(env).getCurrentUrl();
+        softAssert.assertEquals(actualRequestHistoryPageUrl,expectedRequestHistoryPageUrl);
+        softAssert.assertAll();
+        softAssert.assertEquals("Lost Suitcase",rowFirstRequest.getText());
+
+    }
 }
---
+++ src/test/java/com/easybusticket/tests/RequestHistoryNewTicket_US16.java
@@ -0,0 +1,28 @@
+package com.easybusticket.tests;
+
+import com.easybusticket.pages.*;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+@Slf4j
+public class RequestHistoryNewTicket_US16 extends BaseTest {
+
+    @Test
+    public void requestHistoryPageNewTicketTest(){
+
+        // navigate to the login page
+        UserLoginPage userLoginPage = new HomePage().clickToSignIn();
+
+        //navigate to the dashboard page
+        UserDashboardPage userDashboardPage = userLoginPage.login();
+
+        //navigate to the request ticket page
+        SupportTicketPage supportTicketPage = new UserDashboardPage().requestHistory();
+        log.info(" Request History Page loaded ");
+
+        supportTicketPage.requestHistoryNewTicketTest();
+        log.info(" A new ticket was created from the request history page ");
+
+
+    }
+}
