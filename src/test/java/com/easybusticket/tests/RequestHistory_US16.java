a41d95804fd24b4330b9ad80a9a9902f296e3829
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-01-12 10:57:57 +0000

    name changed.request history page

--- src/test/java/com/easybusticket/tests/RequestHistory_US16.java
+++ src/test/java/com/easybusticket/tests/RequestHistory_US16.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
 @Slf4j
-public class US16_TC01 extends BaseTest{
+public class RequestHistory_US16 extends BaseTest{
     @Test
     public void requestCheckTest(){
         // navigate to the login page
