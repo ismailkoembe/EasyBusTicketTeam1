6f16053c7aec943ae551142e6e9416653745dd94
aslierk <asliekmekc@gmail.com>
2024-01-12 14:31:49 +0100

    feat:working on US14 TC01

---
+++ src/test/java/com/easybusticket/tests/TheBookingHistoryAccessor.java
@@ -0,0 +1,34 @@
+package com.easybusticket.tests;
+
+import com.easybusticket.pages.HomePage;
+import com.easybusticket.pages.UserDashboardPage;
+import com.easybusticket.pages.UserLoginPage;
+import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.Test;
+
+@Slf4j
+public class TheBookingHistoryAccessor extends BaseTest{
+
+    @Test
+    public void testBookingHistoryAccessFromMenu() { //I should be able to verify that I can access the booking history page from the booking menu
+
+        // navigate to the login page
+        UserLoginPage userLoginPage = new HomePage().clickToSignIn();
+
+        //navigate to the dashboard page
+        UserDashboardPage userDashboardPage = userLoginPage.login();
+
+        //Locate and hover over the "Booking" menu option.
+        userDashboardPage.dropDownBooking.isEnabled();
+
+        //From the drop-down menu, click on the "Booking History" option.
+        userDashboardPage.bookingHistory.click();
+
+
+
+
+
+
+
+    }
+}
