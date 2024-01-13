b080419d0b1e8daa8bbe43d8520b8485889138ab
ReyhanYildiz <59509001+reyhany@users.noreply.github.com>
2024-01-14 00:13:55 +0100

    Update BlogPageTest_US07.java

--- src/test/java/com/easybusticket/tests/BlogPageTest_US07.java
+++ src/test/java/com/easybusticket/tests/BlogPageTest_US07.java
@@ -16,10 +16,6 @@ public class BlogPageTest_US07 extends BaseTest{
 
         //The user can access all blogspots via blog link in the homepage
 
-<<<<<<< HEAD
-=======
-        //com.easybusticket.pages.BlogPage blogPage = new BlogPage();
->>>>>>> 9f42d9273ed60923cf68efb2e8e20ea3624fc377
         BlogPage blogPage = new HomePage().clickToBlogTitle();
 
     }
