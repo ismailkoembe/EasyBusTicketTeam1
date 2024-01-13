062fda6211d0e6d53694f63643334b18e9d5ee8e
aslierk <asliekmekc@gmail.com>
2024-01-13 09:39:49 +0100

    feat4:created new Page classes related to Payment History feat5 created new Tests related to U25

--- src/test/java/com/easybusticket/tests/PendingPayment_US25.java
+++ src/test/java/com/easybusticket/tests/PendingPayment_US25.java
@@ -4,21 +4,19 @@ import com.easybusticket.pages.AdminDashboardPage;
 import com.easybusticket.pages.AdminPage;
 import com.easybusticket.pages.PendingPaymentPage;
 import lombok.extern.slf4j.Slf4j;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-import java.util.List;
-
 @Slf4j
-public class PaymentHistoryAndOptionsViewerTest_US25 extends BaseTestAdmin {
+public class PendingPayment_US25 extends BaseTestAdmin {
 
     @Test
-    public void paymentHistoryAndOptionsViewerTest() {//Payment History dropdown menu and items should be viewed on the Dashboard sidebar.
+    public void pendingPaymentTest() {//Payment History dropdown menu and items should be viewed on the Dashboard sidebar.
 
         //log in as an administrator and locate the admin Dashboard.
         AdminDashboardPage adminDashboardPage = new AdminPage().adminLogin();
+        adminDashboardPage.clickToPendingPayment();
         PendingPaymentPage pendingPaymentPage = new PendingPaymentPage();
+        pendingPaymentPage.titlePendingPaymentPage();
 
 
     }
