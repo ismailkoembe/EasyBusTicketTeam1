ffacc50593fa9524c2c289ae4c9b3f91a53cc55c
Gamze Erdogan <ckrgamze@gmail.com>
2024-01-14 09:23:10 +0100

    added username and password in the stage.properties. finished all the test on change password page

--- src/test/java/com/easybusticket/tests/UserChangePasswordTest_US18.java
+++ src/test/java/com/easybusticket/tests/UserChangePasswordTest_US18.java
@@ -4,14 +4,15 @@ import com.easybusticket.pages.HomePage;
 import com.easybusticket.pages.UserChangePasswordPage;
 import com.easybusticket.pages.UserDashboardPage;
 import com.easybusticket.pages.UserLoginPage;
+import com.easybusticket.utilities.PropManager;
 import org.testng.annotations.Test;
 
 public class UserChangePasswordTest_US18 extends BaseTest {
     @Test
     public void changePasswordTest() {
         UserLoginPage userLoginPage = new HomePage().clickToSignIn();
-        UserDashboardPage userDashboard = userLoginPage.login("logintesti", "LoginTesti!8");
-        userDashboard.getToChangePasswordPage();
+        UserDashboardPage userDashboardPage = userLoginPage.login(PropManager.getProperties(env,"usernameRegistered"),PropManager.getProperties(env,"passwordRegistered"));
+        userDashboardPage.getToChangePasswordPage();
         new UserChangePasswordPage().clickToChangePasswordButton();
     }
 }
