733ab633eaeb44c7c6a223bb9fcaa6b18e82266b
aslierk <asliekmekc@gmail.com>
2024-01-15 02:30:48 +0100

    feat:created a new Test

--- src/test/java/com/easybusticket/pages/PendingPaymentPage.java
+++ src/test/java/com/easybusticket/pages/PendingPaymentPage.java
@@ -3,9 +3,12 @@ package com.easybusticket.pages;
 import com.easybusticket.utilities.Driver;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 @Slf4j
 public class PendingPaymentPage extends BasePage {
@@ -22,6 +25,22 @@ public class PendingPaymentPage extends BasePage {
 
     @FindBy(xpath = "/html/body/div[1]/div[2]/div/div[1]/div[2]/form[1]/div/input")
     public WebElement trxNumberUsernameSearchBox;
+
+    @FindAll({
+            @FindBy(xpath = "//thead//tr/th[1]"),
+            @FindBy(xpath = "//thead//tr/th[2]"),
+            @FindBy(xpath = "//thead//tr/th[3]"),
+            @FindBy(xpath = "//thead//tr/th[4]"),
+            @FindBy(xpath = "//thead//tr/th[5]"),
+            @FindBy(xpath = "//thead//tr/th[6]"),
+            @FindBy(xpath = "//thead//tr/th[7]"),
+
+    })
+    public List<WebElement> tablePendingPaymentHeaderList;
+
+    @FindBy (xpath = "//td[@class='text-muted text-center']")
+    public  WebElement getTablePendingPaymentNoTicket;
+
 
     public void titlePendingPaymentPage(){
 
--- src/test/java/com/easybusticket/tests/PendingPaymentNoTicketTest_US25.java
+++ src/test/java/com/easybusticket/tests/PendingPaymentNoTicketTest_US25.java
@@ -7,20 +7,18 @@ import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.Test;
 
 @Slf4j
-public class PendingPaymentNoTicketTest_US25 {
+public class PendingPaymentNoTicketTest_US25 extends BaseTestAdmin {
 
     @Test
     public void pendingPaymentNoTicketTest() {
+
         AdminDashboardPage adminDashboardPage = new AdminPage().adminLogin();
         adminDashboardPage.clickToPendingPayment();
         PendingPaymentPage pendingPaymentPage = new PendingPaymentPage();
-        pendingPaymentPage.tablePendingPayment.getText();
+        String actualResultTest = pendingPaymentPage.getTablePendingPaymentNoTicket.getText();
+        softAssert.assertEquals(actualResultTest, "There is no pending payment");
 
-
-
-
-
-
+        softAssert.assertAll();
 
 
     }
