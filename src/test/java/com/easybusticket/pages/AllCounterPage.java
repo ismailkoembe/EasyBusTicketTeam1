e4dc2a473724f558bd342283611cb57902458245
Nazime <kirkgoznazime@gmail.com>
2024-01-15 23:17:51 +0100

    Manage users ve manage active merge edilmicek zaten var

--- src/test/java/com/easybusticket/pages/AllCounterPage.java
+++ src/test/java/com/easybusticket/pages/AllCounterPage.java
@@ -3,8 +3,8 @@ package com.easybusticket.pages;
 import com.easybusticket.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
 
-public class AllCounter {
-    public AllCounter() {
+public class AllCounterPage {
+    public AllCounterPage() {
         PageFactory.initElements(Driver.get("stage"), this);
     }
 }
