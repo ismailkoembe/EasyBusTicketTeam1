7fc1176896ec3179ce2c2f5de964beb94483617e
Ayça Ovalı <145855586+OVALIAYCA@users.noreply.github.com>
2024-01-16 22:43:38 +0000

    About page ve logout methodları step vs eklendi

--- src/test/java/com/easybusticket/pages/UserDashboardPage.java
+++ src/test/java/com/easybusticket/pages/UserDashboardPage.java
@@ -1,6 +1,7 @@
 package com.easybusticket.pages;
 
 import com.easybusticket.utilities.Driver;
+import io.qameta.allure.Step;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -202,19 +203,18 @@ public class UserDashboardPage extends BasePage {
 
     }
 
-
+    @Step("Registered user logout")
     public UserLoginPage logout() {
 
         waitAndClick(dropDownProfile);
         waitAndClick(logoutOption);
         String expectedTitle = "Easy Bus Ticket - Sign In";
         String actualTitle = Driver.get(env).getTitle();
-        softAssert.assertEquals(actualTitle, expectedTitle);
-        softAssert.assertAll();
+        Assert.assertEquals(actualTitle,expectedTitle);
+        log.info("logout method was run");
         return new UserLoginPage();
-
-
     }
+
 
     public SupportTicketPage requestHistory() {
         waitAndClick(dropDownSupportRequest);
