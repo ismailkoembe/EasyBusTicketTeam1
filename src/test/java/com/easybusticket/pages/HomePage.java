27197fb404c5f09dee1e7f3b4e980ae04039e40b
Fatma Gulcan <fatmagulcancandan@gmail.com>
2024-01-17 13:18:32 -0300

    17/01/2024-yeni branch

--- src/test/java/com/easybusticket/pages/HomePage.java
+++ src/test/java/com/easybusticket/pages/HomePage.java
@@ -2,11 +2,9 @@ package com.easybusticket.pages;
 
 import com.easybusticket.utilities.Driver;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.Assert;
 
 /**
  * @author Ismail Koembe
